ebc43644e4fd04fc7f3bc439f6baff4490efc280
Himalee <himalee.tailor@gmail.com>
2018-05-30 17:05:24 +0100

    Refactored tests

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,15 +1,16 @@
 require "display"
 
 describe Display do
+
+  let(:output) { StringIO.new }
+
   it "displays output" do
-    output = StringIO.new
     display = Display.new(output)
     display.present("hello")
     expect(output.string).to eql("hello\n")
   end
 
   it "takes input" do
-    output = StringIO.new
     input = StringIO.new("hello")
     display = Display.new(output, input)
     expect(display.receive).to eql("hello")
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,15 +1,16 @@
 require "player"
 
 describe Player do
+
+  let(:display) { spy(Display) }
+
   context "names of players" do
     it "returns name of player" do
-      display = spy(Display)
       player = Player.new("Himalee", display)
       expect(player.name).to eql("Himalee")
     end
-    
+
     it "returns name of another player" do
-      display = spy(Display)
       player = Player.new("Daisy", display)
       expect(player.name).to eql("Daisy")
     end
@@ -17,19 +18,16 @@ describe Player do
 
   context "players options" do
     it "player can choose rock" do
-      display = spy(Display)
       player = Player.new("Himalee", display)
       expect(player.rock).to eql("rock")
     end
 
     it "player can choose paper" do
-      display = spy(Display)
       player = Player.new("Himalee", display)
       expect(player.paper).to eql("paper")
     end
 
     it "player can choose scissors" do
-      display = spy(Display)
       player = Player.new("Himalee", display)
       expect(player.scissors).to eql("scissors")
     end
