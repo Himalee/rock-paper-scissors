49a36fe699b6900893b65ef82ce4e8e1c905c7db
Himalee <himalee.tailor@gmail.com>
2018-06-12 10:46:54 +0100

    Refactors

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -2,26 +2,8 @@ require_relative "player"
 
 class ComputerPlayer < Player
 
-<<<<<<< HEAD
   def get_input
     random_hand_gesture
-=======
-  attr_reader :player_input, :player_name
-
-  def get_input
-    @display.present(Messages.new.player_prompt(player_name))
-    randomise
-    display_input
-  end
-
-  def player_name
-    "Computer"
-  end
-
-  def randomise
-    options = ["rock", "paper", "scissors"]
-    @player_input = options.sample
->>>>>>> a4d31d74287bdcb822d1cba4b6714c2278ae6e98
   end
 
   def random_hand_gesture
--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,7 @@ require_relative "player"
 require_relative "human_player"
 require_relative "computer_player"
 require_relative "rules"
+require_relative "move_validator"
 
 class Game
 
@@ -15,19 +16,10 @@ class Game
   end
 
   def play
-<<<<<<< HEAD
     @display.welcome_users
     player_turn(@player_one)
     player_turn(@player_two)
     result
-=======
-    welcome_users
-    @player_one.get_input
-    @player_two.get_input
-    result = option_one_result(@player_one.player_input, @player_two.player_input)
-    present_winner(result)
-    play_again?
->>>>>>> a4d31d74287bdcb822d1cba4b6714c2278ae6e98
   end
 
   def player_turn(player)
@@ -44,21 +36,12 @@ class Game
     end
   end
 
-<<<<<<< HEAD
   def winner
     result = @rules.winner?(:player_one => @player_one.player_input, :player_two => @player_two.player_input)
     if result == :player_one
       @display.present_winner(@player_one.name)
     else
       @display.present_winner(@player_two.name)
-=======
-  def present_winner(result)
-    if result == "draw"
-      @display.present(@messages.draw)
-    elsif result == "win"
-      @display.present(@messages.winning_message(@player_one.player_name))
-    else
-      @display.present(@messages.winning_message(@player_two.player_name))
     end
   end
 
@@ -67,7 +50,6 @@ class Game
     answer = @display.receive
     until answer != "y"
       play
->>>>>>> a4d31d74287bdcb822d1cba4b6714c2278ae6e98
     end
   end
 end
